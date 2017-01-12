7835e0532a7e2e0a59d3b5fcc01ddca137b96ea2
Norbert Rozmus <nrozmus@insolutions.pl>
2017-01-12 21:22:08 +0100

    Add first custom resp

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-
+const slackreposonse = require('./lunch/slackresponse.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
@@ -10,12 +10,18 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
-  res.send('Hello world')
+  res.send('Hello world v2')
 });
 
 app.post('/test',(req,res) => {
 
     let text = req.body.text;
+    let  textResponse = '';
+    if(text == 'about'){
+        textResponse = slackreposonse.about();
+    }else if(text == 'help'){
+        textResponse = slackreposonse.help();
+    }
 
     if(text == 'error'){
         res.send("error message");
@@ -23,7 +29,7 @@ app.post('/test',(req,res) => {
 
     let returnValue = {
         response_type: 'in_channel',
-        text: 'to jest to!'        
+        text: textResponse        
     };
 
     res.json(returnValue);
---
+++ lunch/slackresponse.js
@@ -0,0 +1,11 @@
+exports.about = function(){
+    return 'Aplikacja służąca do zażądzania zamówieniami na lunch';
+};
+
+exports.help = function(){
+    return 'Lista komend: ' +
+            'About - informacje o aplikacji' 
+        +  'Menu - przedstawia aktualne menu'
+        +  'Phone - dane adresowe do restauracji'
+        +  'Random - wybiera losową potrawę z menu';
+}
