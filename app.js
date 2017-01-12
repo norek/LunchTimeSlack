9de9e61e76e5ae793d50ab1be07b7a079f54d600
Norbert Rozmus <nrozmus@insolutions.pl>
2017-01-12 21:00:09 +0100

    add first test method

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-var express = require('express')
-var app = express()
+'use strict'
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -7,6 +13,24 @@ app.get('/', function (req, res) {
   res.send('Hello world')
 });
 
+app.post('/test',(req,res) => {
+
+    let text = req.body.text;
+
+    if(text == 'error'){
+        res.send("error message");
+    }
+
+    let returnValue = {
+        response_type: 'in_channel',
+        text: 'to jest to!'        
+    };
+
+    res.json(returnValue);
+
+    console.log(text);
+
+});
 
 
 app.get('/help', function (req, res) {
