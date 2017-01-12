98475f010a4a2e4dd1ef8ad140fff43e36f8ce9e
Norbert Rozmus <nrozmus@insolutions.pl>
2017-01-12 20:28:38 +0100

    add ProcFile and fix app.js

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 var express = require('express')
 var app = express()
 
-app.get('/help', function (req, res) {
-  res.send('Hello this is LUNCH TIME! prepare your anus')
-})
+app.set('port', (process.env.PORT || 5000));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.get('/', function (req, res) {
+  res.send('Hello world')
+});
+
+
+
+app.get('/help', function (req, res) {
+  res.send('help from hello world')
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
